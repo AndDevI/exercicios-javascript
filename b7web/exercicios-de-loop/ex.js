7b9a5ead2f36325987b7dd6e56122e4433ba7474
Andrew <andrewrgpires@gmail.com>
2024-09-16 11:22:01 -0300

    resolução do exercicio 1

--- b7web/exercicios-de-loop/ex.js
+++ b7web/exercicios-de-loop/ex.js
@@ -1,10 +1,20 @@
 // 1. Faça um loop que mostre todas as frutas 
-// let fruits = ["Maça", "Uva", "Banana"]
+let fruits = ["Maça", "Uva", "Banana"]
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+};
+
+// Versão resumida:
+
+for (let i in fruits) {
+    console.log(fruits[i])
+};
 
 // 2. Conte de 1 até 100 através de um WHILE
-let number = 1
+let number = 1;
 
 while (number <= 100) {
     console.log(number);
     number++;
-}
+}; 
